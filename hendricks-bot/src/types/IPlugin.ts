14bd7e37e407771c0e509635b5b8d00fb96dd617
Tomer Atar <kenyweb7@gmail.com>
2022-10-21 18:09:31 +0300

    fixed abs import

--- hendricks-bot/src/types/IPlugin.ts
+++ hendricks-bot/src/types/IPlugin.ts
@@ -1,8 +1,8 @@
 import ICommand from './ICommand';
 import { PluginManager } from '@sgty/plugin-system';
 import { Client } from 'discord.js';
-import CommandRegistry from 'src/CommandRegistry';
-import { Events } from 'src/events';
+import CommandRegistry from '../CommandRegistry';
+import { Events } from '../events';
 
 export default interface IPlugin {
 	id: string;
